f0ac4b9e156545fd98003c1d9b104ec92e126095
openshift-ops <bishesh.halloween959@gmail.com>
2022-06-02 03:36:41 +0530

    adding tests

---
+++ hello-jenkins-maven/src/test/java/com/example/maven/controller/HelloJenkinsTest.java
@@ -0,0 +1,32 @@
+package com.example.maven.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+/**
+ * @author Bishesh
+ */
+@WebMvcTest
+public class HelloJenkinsTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHelloJenkins ()throws Exception{
+        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.get("/api/jenkins"));
+
+        result.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("HelloJenkins from maven"));
+
+    }
+
+}
